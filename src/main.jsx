33e3202723f8ac1243adbf5181c05bc4d8fde0cd
Maggie <xungirl1024@gmail.com>
2026-03-24 18:48:03 -0700

    feat: configure for GitHub Pages static hosting
    - Switch BrowserRouter to HashRouter (GitHub Pages compatible)
    - Set vite base to /sudoku-website/
    - Add gh-pages deploy script

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { GameProvider } from './context/GameContext'
 import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 
-console.log('[Sudoku] App starting...')
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ErrorBoundary>
-    <BrowserRouter>
+    <HashRouter>
       <GameProvider>
         <App />
       </GameProvider>
-    </BrowserRouter>
+    </HashRouter>
   </ErrorBoundary>
 )
-
-console.log('[Sudoku] App rendered.')
